f83b228c3b3669ad52f2be314c7bed0d60528cf2
Nicolas <nicolasgfalabella@gmail.com>
2024-04-02 21:45:18 -0300

    Cambios de estilos

--- src/app/api/auth/loguin/route.js
+++ src/app/api/auth/loguin/route.js
@@ -1,3 +1,13 @@
-export function POST(request) {
+import { connectMongoDB } from "@/app/libs/mongodb";
+import { NextResponse } from "next/server";
 
+export async function POST(request) {
+    try{
+        await connectMongoDB();
+
+        const body = await request.json()
+    }
+    catch{
+
+ }
 }
