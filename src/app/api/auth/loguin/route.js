647cfba02e8d8eeffc0cc6770de07f29af60e47d
Nicolas <nicolasgfalabella@gmail.com>
2024-04-03 10:55:40 -0300

    Avance sobre autenticacion

--- src/app/api/auth/loguin/route.js
+++ src/app/api/auth/loguin/route.js
@@ -1,11 +1,45 @@
 import { connectMongoDB } from "@/app/libs/mongodb";
+import User from "@/app/models/User";
+import { messages } from "@/utils/message";
 import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs"
 
 export async function POST(request) {
     try{
         await connectMongoDB();
 
         const body = await request.json()
+        const { email , password } = body
+
+        //validacion que no falten datos en el loguin
+        if( !email || !password ){
+            return NextResponse.json({
+                message : messages.error.needProps
+            },{
+                status : 400
+            })
+        }
+
+        const userFind = await User.findOne({email})
+
+        //validacion de que el usuario existe
+        if(!userFind){
+            return NextResponse.json({
+                message: messages.error.userNotFound
+            })
+        }
+
+        const isCorrect = await bcryptjs.compare(
+            password,
+            userFind.password
+        )
+        
+
+        if(!isCorrect){
+            return NextResponse.json({
+                messages: messages.error.incorrectPassword
+            })
+        }
     }
     catch{
 
--- src/utils/message.js
+++ src/utils/message.js
@@ -4,6 +4,8 @@ export const messages = {
         emailNotValid : "El email no es valido",
         passwordsNotMatch : "Las contraseñas no coinciden",
         emailExist: "El email ya esta registrado",
+        userNotFound: "El emailno ha sido encontrado",
+        incorrectPassword: "la contraseña es incorrecta",
         errorDefault : "Algo salio mal"
     },
     succes: {
